86c9aa604329b82f09ab09ec52f5dbc0a5c60750
Reesa Dayani <reesadayani@gmail.com>
2024-11-19 00:34:57 -0500

    Add files via upload

---
+++ superset_config.py
@@ -0,0 +1,11 @@
+SECRET_KEY = "O+Ot7GuqPudV7xWuW2EtMZ+nG0edNy4SliRWrU3tsuY4XLdwlGJQqOlL"
+SQLALCHEMY_DATABASE_URI = 'mysql://testuser:testuser@mysql:3306/superset_db'  # Use 'mysql' as hostname
+
+DEBUG = True
+
+
+
+FEATURE_FLAGS = {
+    "ALLOW_FILE_UPLOAD": True
+    
+}
