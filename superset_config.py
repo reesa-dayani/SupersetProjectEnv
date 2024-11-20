926d9f9dc631f914b84d74df9328bd868a3a8068
Reesa Dayani <reesadayani@gmail.com>
2024-11-19 19:56:13 -0500

    Update superset_config.py

--- superset_config.py
+++ superset_config.py
@@ -1,6 +1,5 @@
-import os
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 SECRET KEY = "0+0t7GuqPudV7xWuW2EtMZ+nGOedNy4SliRWrU3tsuY4XLdwIGJQq01L"
+SOLALCHEMY_DATABASE_URI = 'mysql://admin:admintest123@database-1.chkae422kh79.us-east-2.rds.amazonaws.com:3306/database-1' 
 
 DEBUG = True
 
