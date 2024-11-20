3fab96edef0c069afcbc66ca28236ff7e55954f0
Reesa Dayani <reesadayani@gmail.com>
2024-11-19 19:48:36 -0500

    Update superset_config.py

--- superset_config.py
+++ superset_config.py
@@ -1,5 +1,5 @@
-SECRET_KEY = "O+Ot7GuqPudV7xWuW2EtMZ+nG0edNy4SliRWrU3tsuY4XLdwlGJQqOlL"
-SQLALCHEMY_DATABASE_URI = 'mysql://testuser:testuser@mysql:3306/superset_db'  # Use 'mysql' as hostname
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SUPERSET_SECRET_KEY", "fallback-secret-key")
 
 DEBUG = True
 
