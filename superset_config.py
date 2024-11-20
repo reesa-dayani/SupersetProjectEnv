7eb20117cd972f3a1dacf3002a864b71b95dff23
Reesa Dayani <reesadayani@gmail.com>
2024-11-19 20:00:53 -0500

    Update superset_config.py

--- superset_config.py
+++ superset_config.py
@@ -1,4 +1,4 @@
-SECRET KEY = "0+0t7GuqPudV7xWuW2EtMZ+nGOedNy4SliRWrU3tsuY4XLdwIGJQq01L"
+SECRET_KEY = "0+0t7GuqPudV7xWuW2EtMZ+nGOedNy4SliRWrU3tsuY4XLdwIGJQq01L"
 SOLALCHEMY_DATABASE_URI = 'mysql://admin:admintest123@database-1.chkae422kh79.us-east-2.rds.amazonaws.com:3306/database-1' 
 
 DEBUG = True
